9987ceffc1c2188bd0dc2111c91b187d16ef755e
cyn-rus <cyn.rusadi@gmail.com>
2021-03-19 22:46:03 +0700

    class player

---
+++ src/headers/Player.hpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+#define MAXCAPACITY 50;
+
+using namespace std;
+
+class Player {
+    protected:
+        string name;
+        bool isEngiActive; // kyknya masuk game aja
+        // Engimon activeEngi; kyknya masuk game juga
+        // Inventory<Engimon>[] listEngimon;
+        // Inventory<SkillItem>[] listSkill;
+        int totalInvent;
+        int maxCapacity;
+
+    public:
+        Player(string _name);
+        bool isEngimonActive() const;
+        // Engimon getActiveEngimon() const;
+        int getTotalInventory() const;
+        // void switchEngimon(Engimon engi);
+};
+
+
+#endif
---
+++ src/impl/Player.cpp
@@ -0,0 +1,26 @@
+#include "Player.hpp"
+
+Player::Player(string _name) : name(_name), isEngiActive(false), totalInvent(0)  {
+    maxCapacity = MAXCAPACITY;
+}
+
+bool Player::isEngimonActive() const {
+    return isEngiActive;
+}
+
+// Engimon Player::getActiveEngimon() const {
+//     return activeEngi;
+// }
+
+int Player::getTotalInventory() const {
+    return totalInvent;
+}
+
+// void Player::switchEngimon(Engimon engi) {
+//     if (isEngiActive) {
+        
+//     }
+//     else {
+        
+//     }
+// }
