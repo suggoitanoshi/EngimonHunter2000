0707a6ac4e005a1e21fb655203bd7fa3c2e0b5b9
Josep Marcello <jspmarcello@live.com>
2021-04-11 11:42:58 +0700

    Update pom.xml to use junit and update .gitignore

--- src/test/java/EngimonHunter2000/SkillTest.java
+++ src/test/java/EngimonHunter2000/SkillTest.java
@@ -1,30 +1,30 @@
 package EngimonHunter2000;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class SkillTest {
     @Test
     public void constructorWithEnum() throws EngimonHunter2000Exception {
         Skill s = new Skill("THUNDAAA", 40, Elements.El.ELECTRIC);
-        Assertions.assertNotNull(s);
         assertAll("Harusnya menghasilkan skill default (THUNDAAA, power: 40, Elements: Electric)",
+            () -> assertNotNull(s),
             () -> assertEquals("THUNDAAA", s.getName()),
             () -> assertEquals(40, s.getBasePower()),
-            () -> assertEquals(Elements.El.ELECTRIC, s.getElements())
+            () -> assertEquals(1, s.getElements().size()),
+            () -> assertEquals(Elements.El.ELECTRIC, s.getElements().toArray()[0])
         );
     }
 
+    @Test
     public void constructorWithString() throws EngimonHunter2000Exception {
-        Skill s = new Skill("THUNDAAA", 40, "Electric");
-        Assertions.assertNotNull(s);
+        Skill s = new Skill("THUNDAAA", 40, "ElecTRic");
         assertAll("Harusnya menghasilkan skill default (THUNDAAA, power: 40, Elements: Electric)",
+            () -> assertNotNull(s),
             () -> assertEquals("THUNDAAA", s.getName()),
             () -> assertEquals(40, s.getBasePower()),
-            () -> assertEquals(Elements.El.ELECTRIC, s.getElements())
+            () -> assertEquals(1, s.getElements().size()),
+            () -> assertEquals(Elements.El.ELECTRIC, s.getElements().toArray()[0])
         );
     }
 }
