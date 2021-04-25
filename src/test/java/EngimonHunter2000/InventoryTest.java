4cd2ee44fe9bfeabe26ac7054450871038be2af2
Josep Marcello <jspmarcello@live.com>
2021-04-25 16:43:39 +0700

    update InventoryTest to follow clang-format

--- src/test/java/EngimonHunter2000/InventoryTest.java
+++ src/test/java/EngimonHunter2000/InventoryTest.java
@@ -9,91 +9,96 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class InventoryTest{
-  @BeforeAll
-  public static void initAll(){
-    Inventory.setMax(5);
-  }
-
-  @AfterEach
-  public void cleanup(){
-    Inventory.resetItems();
-  }
-
-  @Test
-  public void constructOne(){
-    Inventory<Integer> i1 = new Inventory<Integer>();
-    assertAll("Konstruk satu inventory",
-      () -> assertEquals(0, i1.getItemCount()));
-  }
-
-  @Test
-  public void constructTwo(){
-    Inventory<Integer> i1 = new Inventory<Integer>();
-    Inventory<Float> i2 = new Inventory<Float>();
-    assertAll("Konstruk dua inventory",
-      () -> assertEquals(0, i1.getItemCount()),
-      () -> assertEquals(0, i2.getItemCount()));
-  }
-
-  @Test
-  public void addOnlyOneInventory() throws InventoryException{
-    Inventory<Integer> i1 = new Inventory<Integer>();
-    i1.addItem(0);
-    assertAll("Tambah item ke satu inventory",
-      () -> assertEquals(1, i1.getItemCount()));
-  }
-
-  @Test
-  public void addTwoInventories() throws InventoryException{
-    Inventory<Integer> i1 = new Inventory<Integer>();
-    Inventory<Float> i2 = new Inventory<Float>();
-    i1.addItem(0);
-    i2.addItem(0f);
-    assertAll("Tambah item ke dua inventory",
-      () -> assertEquals(2, i1.getItemCount()),
-      () -> assertEquals(2, i2.getItemCount()));
-  }
-
-  @Test
-  public void addOneLimit() throws InventoryException{
-    Inventory<Integer> i1 = new Inventory<Integer>();
-    for(int i = 0; i < 5; i++){
-      i1.addItem(i);
+public class InventoryTest {
+    @BeforeAll
+    public static void initAll() {
+        Inventory.setMax(5);
     }
-    assertThrows(InventoryException.class, () -> i1.addItem(6));
-  }
 
-  @Test
-  public void addTwoLimit() throws InventoryException{
-    Inventory<Integer> i1 = new Inventory<Integer>();
-    Inventory<Float> i2 = new Inventory<Float>();
-    for(int i = 0; i < 5; i++){
-      i1.addItem(i);
+    @AfterEach
+    public void cleanup() {
+        Inventory.resetItems();
     }
-    assertThrows(InventoryException.class, () -> i2.addItem(6f));
-  }
 
-  @Test
-  public void removeOneEmpty(){
-    Inventory<Integer> i1 = new Inventory<Integer>();
-    assertThrows(InventoryException.class, () -> i1.removeItem(0));
-  }
+    @Test
+    public void constructOne() {
+        Inventory<Integer> i1 = new Inventory<Integer>();
+        assertAll("Konstruk satu inventory",
+            () -> assertEquals(0, i1.getItemCount()));
+    }
 
-  public void removeOneValid() throws InventoryException{
-    Inventory<Integer> i1 = new Inventory<Integer>();
-    i1.addItem(0);
-    i1.removeItem(0);
-    assertAll("Penghapusan invalid",
-      () -> assertEquals(0, i1.getItemCount()),
-      () -> assertThrows(InventoryException.class, () -> i1.at(0)));
-  }
+    @Test
+    public void constructTwo() {
+        Inventory<Integer> i1 = new Inventory<Integer>();
+        Inventory<Float> i2 = new Inventory<Float>();
+        assertAll("Konstruk dua inventory",
+            ()
+                -> assertEquals(0, i1.getItemCount()),
+            () -> assertEquals(0, i2.getItemCount()));
+    }
 
-  public void removeTwoInvalid() throws InventoryException{
-    Inventory<Integer> i1 = new Inventory<Integer>();
-    Inventory<Float> i2 = new Inventory<Float>();
-    i1.addItem(0);
-    assertAll("Penghapusan invalid pada inventory ke-2",
-      () -> assertThrows(InventoryException.class, () -> i2.removeItem(0f)));
-  }
+    @Test
+    public void addOnlyOneInventory() throws InventoryException {
+        Inventory<Integer> i1 = new Inventory<Integer>();
+        i1.addItem(0);
+        assertAll("Tambah item ke satu inventory",
+            () -> assertEquals(1, i1.getItemCount()));
+    }
+
+    @Test
+    public void addTwoInventories() throws InventoryException {
+        Inventory<Integer> i1 = new Inventory<Integer>();
+        Inventory<Float> i2 = new Inventory<Float>();
+        i1.addItem(0);
+        i2.addItem(0f);
+        assertAll("Tambah item ke dua inventory",
+            ()
+                -> assertEquals(2, i1.getItemCount()),
+            () -> assertEquals(2, i2.getItemCount()));
+    }
+
+    @Test
+    public void addOneLimit() throws InventoryException {
+        Inventory<Integer> i1 = new Inventory<Integer>();
+        for (int i = 0; i < 5; i++) {
+            i1.addItem(i);
+        }
+        assertThrows(InventoryException.class, () -> i1.addItem(6));
+    }
+
+    @Test
+    public void addTwoLimit() throws InventoryException {
+        Inventory<Integer> i1 = new Inventory<Integer>();
+        Inventory<Float> i2 = new Inventory<Float>();
+        for (int i = 0; i < 5; i++) {
+            i1.addItem(i);
+        }
+        assertThrows(InventoryException.class, () -> i2.addItem(6f));
+    }
+
+    @Test
+    public void removeOneEmpty() {
+        Inventory<Integer> i1 = new Inventory<Integer>();
+        assertThrows(InventoryException.class, () -> i1.removeItem(0));
+    }
+
+    public void removeOneValid() throws InventoryException {
+        Inventory<Integer> i1 = new Inventory<Integer>();
+        i1.addItem(0);
+        i1.removeItem(0);
+        assertAll("Penghapusan invalid",
+            ()
+                -> assertEquals(0, i1.getItemCount()),
+            () -> assertThrows(InventoryException.class, () -> i1.at(0)));
+    }
+
+    public void removeTwoInvalid() throws InventoryException {
+        Inventory<Integer> i1 = new Inventory<Integer>();
+        Inventory<Float> i2 = new Inventory<Float>();
+        i1.addItem(0);
+        assertAll("Penghapusan invalid pada inventory ke-2",
+            ()
+                -> assertThrows(
+                    InventoryException.class, () -> i2.removeItem(0f)));
+    }
 }
